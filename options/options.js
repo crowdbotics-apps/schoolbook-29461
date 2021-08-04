03cf98146d22613f06249349c3e00e6903cda51a
Crowdbotics <team@crowdbotics.com>
2021-08-04 17:02:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "schoolbook_29461Identifier",
+  url: "https://schoolbook_29461Identifier.botics.co",
+  api: "https://schoolbook_29461Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
